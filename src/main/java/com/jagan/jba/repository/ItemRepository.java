697be591a0ace52c7094eb641c39cccab356284d
Jaganraji <emailjaganraji@gmail.com>
2015-07-15 18:38:06 -0400

    Commit Tutorial 18.

--- src/main/java/com/jagan/jba/repository/ItemRepository.java
+++ src/main/java/com/jagan/jba/repository/ItemRepository.java
@@ -2,13 +2,16 @@ package com.jagan.jba.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jagan.jba.entity.Blog;
-import com.jagan.jba.entity.Item;;
+import com.jagan.jba.entity.Item;
+
 
 public interface ItemRepository extends JpaRepository<Item, Integer>{
 
-	List<Item> findByBlog(Blog blog);
+	List<Item> findByBlog(Blog blog, Pageable pageable);
+	
 
 }
