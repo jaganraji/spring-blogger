bb39f804282d4353ea4236cc59c5563b2f02fe74
Jaganraji <emailjaganraji@gmail.com>
2015-07-18 19:00:17 -0400

    Committing after tutorial 29.

--- src/main/java/com/jagan/jba/controller/UserController.java
+++ src/main/java/com/jagan/jba/controller/UserController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.jagan.jba.entity.Blog;
 import com.jagan.jba.entity.User;
+import com.jagan.jba.service.BlogService;
 import com.jagan.jba.service.UserService;
 
 @Controller
@@ -18,11 +20,18 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
+	@Autowired
+	private BlogService blogService;
+
 	@ModelAttribute("user")
-	public User construct()
-	{
+	public User constructUser() {
 		return new User();
+	}
+
+	@ModelAttribute("blog")
+	public Blog constructBlog() {
+		return new Blog();
 	}
 
 	@RequestMapping("/users")
@@ -36,26 +45,30 @@ public class UserController {
 		model.addAttribute("user", userService.findOneWithBlogs(id));
 		return "user-detail";
 	}
-	
+
 	@RequestMapping("/register")
 	public String showRegister() {
 		return "user-register";
 	}
-	
-	
-	@RequestMapping(value="/register", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String doRegister(@ModelAttribute("user") User user) {
 		userService.save(user);
 		return "redirect:/register.html?success=true";
 	}
-	
+
 	@RequestMapping("/account")
 	public String account(Model model, Principal principal) {
 		String name = principal.getName();
 		model.addAttribute("user", userService.findOneWithBlogs(name));
 		return "user-detail";
 	}
-	
-	
-	
+
+	@RequestMapping(value = "/account", method = RequestMethod.POST)
+	public String doAddBlog(@ModelAttribute("blog") Blog blog, Principal principal) {
+		String name = principal.getName();
+		blogService.save(blog, name);
+		return "redirect:/account.html";
+	}
+
 }
---
+++ src/main/java/com/jagan/jba/service/BlogService.java
@@ -0,0 +1,27 @@
+package com.jagan.jba.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jagan.jba.entity.Blog;
+import com.jagan.jba.entity.User;
+import com.jagan.jba.repository.BlogRepository;
+import com.jagan.jba.repository.UserRepository;
+
+@Service
+public class BlogService {
+	
+	@Autowired
+	private BlogRepository blogRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public void save(Blog blog, String name) {
+		User user = userRepository.findByName(name);
+		blog.setUser(user);
+		blogRepository.save(blog);
+	}
+	
+
+}
